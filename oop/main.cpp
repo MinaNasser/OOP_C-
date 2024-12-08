3254444cbf5d8bc2e8d251d1847f8f85629c89ab
MinaAl5al <minanasser82018@gmail.com>
2024-12-08 22:45:32 +0200

    netSalary

--- oop/main.cpp
+++ oop/main.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 
 using namespace std;
-class Car{
+class Car
+{
     int speed;
-    
+    char Color[20];
+    void moveForward()
+    {
+        cout << "move forward" << endl;
+    }
 
+    void breakCar(){
+        cout << "break" << endl;
+    }
 };
+
+int add(int x, int y){
+    return x+y;
+}
+float calaculatNetSalary(float salary, float bounas=150){
+    return salary + bounas;
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int a= add(1,2);
+    float netSalary = calaculatNetSalary(1000);
+    float netSalary2 = calaculatNetSalary(1000,500);
+    cout<<a<<endl;
+    cout<<netSalary<<endl;
+    cout<<netSalary2<<endl;
     return 0;
 }
